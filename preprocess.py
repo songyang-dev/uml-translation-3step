508c62553c25a9870909b7e8ae3ce7f4cc394e47
Song Yang <songyang1218@gmail.com>
2021-11-23 14:23:57 -0500

    Init

---
+++ preprocess.py
@@ -0,0 +1,7 @@
+# Preprocess the data for use
+
+# Algorithm
+# 1. Classification of the kind of fragment
+# 2. Syntax parsing and use of heuristics to extract salient words
+# 3. Assembly of fragments into one model
+
