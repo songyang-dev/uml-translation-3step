d273fa62dc7aca8368f1b71a16f946722968cc71
Song Yang <songyang1218@gmail.com>
2022-02-17 21:21:47 -0500

    🚧 Isolated coref resolution

--- extraction/preprocess.py
+++ extraction/preprocess.py
@@ -7,8 +7,9 @@ The sentences then have all coreferences resolved.
 
 from sys import argv
 
-# import spacy, coreferee
-import spacy
+import spacy, coreferee.data_model
+
+# import spacy
 
 if __name__ == "__main__":
     # if len(argv) != 3:
@@ -17,7 +18,7 @@ if __name__ == "__main__":
     pass
 
 nlp = spacy.load("en_core_web_sm")
-# nlp.add_pipe("coreferee")
+nlp.add_pipe("coreferee")
 
 
 def preprocess(text: str):
@@ -25,8 +26,81 @@ def preprocess(text: str):
     Substitute all the coreferences. Then split the sentences.
     """
     doc = nlp(text)
-    for s in doc.sents:
-        print(s)
+
+    chains: coreferee.data_model.ChainHolder = doc._.coref_chains
+
+    substitutions = {}
+
+    for chain in chains:
+        chain: coreferee.data_model.Chain
+        most_specific_mention = chain[chain.most_specific_mention_index]
+        for mention in chain:
+            mention: coreferee.data_model.Mention
+
+            # the original mention
+            if mention == most_specific_mention:
+                continue
+
+            # do not substitute possessive articles his, her, its
+            if doc[mention.root_index].text in ["his", "her", "its"]:
+                continue
+
+            # record changes of all mentions to be the most specific one
+            # root, (source indexes, new indexes)
+            substitutions[mention.root_index] = (
+                mention.token_indexes,
+                most_specific_mention.token_indexes,
+            )
+
+    # perform substitutions
+    result = {0: ""}  # sentence id, sentence str
+    carry_over = {}  # token id, replacement str
+    sent_id = 0  # current sentence
+
+    # cycle through the original text
+    for token in doc:
+
+        # there is a substitution
+        if token.i in substitutions:
+            to_be_replaced, replacement = substitutions[token.i]
+            if len(to_be_replaced) > 1:
+                # add other indices to be carried over
+                for extra_id in to_be_replaced[1:]:
+                    carry_over[extra_id] = conjunctive_addition(
+                        [doc[w].text for w in replacement]
+                    )
+
+            if len(to_be_replaced) == 0:
+                raise Exception("No source tokens for substitution")
+
+            result[sent_id] += conjunctive_addition([doc[w].text for w in replacement])
+            result[sent_id] += " "
+
+        # carry over substitution
+        elif token.i in carry_over:
+            result[sent_id] += carry_over[token.i]
+
+        # no substitution
+        else:
+            result[sent_id] += token.text
+            result[sent_id] += " "
+
+        # next sentence
+        if token.is_sent_end:
+            sent_id += 1
+            result[sent_id] = ""
+
+    return result
+
+
+def conjunctive_addition(words: list[str]):
+    """
+    item1, item2, ... and last item
+    """
+    if len(words) == 1:
+        return words[0]
+    else:
+        return ",".join(words[:-1]) + " and " + words[-1]
 
 
 if __name__ == "__main__":
